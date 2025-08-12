3615bdeaccf580943e52ea8a466963f22347686f
Marko Hlavaty <marko.hlavaty@gmail.com>
2025-08-12 14:23:47 +0200

    Add parameters for minimum buffer length and padding at the start of VAD segment

--- whisper_streaming/vac_online_processor.py
+++ whisper_streaming/vac_online_processor.py
@@ -14,10 +14,12 @@ class VACOnlineASRProcessor(OnlineProcessorInterface):
     When it detects end of speech (non-voice for 500ms), it makes OnlineASRProcessor to end the utterance immediately.
     '''
 
-    def __init__(self, online_chunk_size, online):
+    def __init__(self, online_chunk_size, online, min_buffered_length=1, start_padding=0.5):
         self.online_chunk_size = online_chunk_size
-
         self.online = online
+
+        self.min_buffered_frames = int(min_buffered_length * self.SAMPLING_RATE)
+        self.start_padding_frames = int(start_padding * self.SAMPLING_RATE)
 
         # VAC:
         import torch
@@ -48,7 +50,7 @@ class VACOnlineASRProcessor(OnlineProcessorInterface):
         self.audio_buffer = np.append(self.audio_buffer, audio)
         if res is not None:
             if 'start' in res:
-                res['start'] = max(0, res['start'] - 8000)
+                res['start'] = max(0, res['start'] - self.start_padding_frames)
             frame = list(res.values())[0] - self.buffer_offset
             if 'start' in res and 'end' not in res:
                 self.status = 'voice'
@@ -64,8 +66,9 @@ class VACOnlineASRProcessor(OnlineProcessorInterface):
                 self.online.insert_audio_chunk(send_audio)
                 self.current_online_chunk_buffer_size += len(send_audio)
                 self.is_currently_final = True
-                self.audio_buffer = self.audio_buffer[frame:]
-                self.buffer_offset += frame
+                keep_frames = min(len(self.audio_buffer) - frame, self.min_buffered_frames)
+                self.audio_buffer = self.audio_buffer[-keep_frames:]
+                self.buffer_offset += len(self.audio_buffer) - keep_frames
             else:
                 beg = res["start"]-self.buffer_offset
                 end = res["end"]-self.buffer_offset
@@ -75,8 +78,9 @@ class VACOnlineASRProcessor(OnlineProcessorInterface):
                 self.online.insert_audio_chunk(send_audio)
                 self.current_online_chunk_buffer_size += len(send_audio)
                 self.is_currently_final = True
-                self.audio_buffer = self.audio_buffer[end:]
-                self.buffer_offset += end
+                keep_frames = min(len(self.audio_buffer) - end, self.min_buffered_frames)
+                self.audio_buffer = self.audio_buffer[-keep_frames:]
+                self.buffer_offset += len(self.audio_buffer) - keep_frames
         else:
             if self.status == 'voice':
                 self.online.insert_audio_chunk(self.audio_buffer)
@@ -86,9 +90,8 @@ class VACOnlineASRProcessor(OnlineProcessorInterface):
             else:
                 # We keep 1 second because VAD may later find start of voice in it.
                 # But we trim it to prevent OOM. 
-                self.audio_buffer = self.audio_buffer[-self.SAMPLING_RATE:]
-                self.buffer_offset += max(0,len(self.audio_buffer)-self.SAMPLING_RATE)
-
+                self.audio_buffer = self.audio_buffer[-self.min_buffered_frames:]
+                self.buffer_offset += max(0,len(self.audio_buffer) - self.min_buffered_frames)
 
     def process_iter(self):
         if self.is_currently_final:
